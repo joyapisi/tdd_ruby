c4fb5690fb30ade7003f7cd31c89c4d49acbb4de
Joy Phoebe <joyapisi@gmail.com>
2023-06-29 13:02:37 +0300

    update tests

--- classes/solver.rb
+++ classes/solver.rb
@@ -15,32 +15,32 @@
 #   end
 # end
 
-# def reverse_string(str)
-#   reversed = ''
-#   i = str.length - 1
+def reverse_string(str)
+  reversed = ''
+  i = str.length - 1
 
-#   while i >= 0
-#     reversed += str[i]
-#     i -= 1
-#   end
+  while i >= 0
+    reversed += str[i]
+    i -= 1
+  end
 
-#   reversed
-# end
+  reversed
+end
 
-# # Example usage:
-# input = 'Hello, World!'
-# reversed_string = reverse_string(input)
-# puts reversed_string
+# Example usage:
+input = 'Hello, World!'
+reversed_string = reverse_string(input)
+puts reversed_string
 
-# # fizzbuzz
-# def fizzbuzz(number)
-#   if (number % 3).zero? && (number % 5).zero?
-#     'fizzbuzz'
-#   elsif (number % 3).zero?
-#     'fizz'
-#   elsif (number % 5).zero?
-#     'buzz'
-#   else
-#     number.to_s
-#   end
-# end
+# fizzbuzz
+def fizzbuzz(number)
+  if (number % 3).zero? && (number % 5).zero?
+    'fizzbuzz'
+  elsif (number % 3).zero?
+    'fizz'
+  elsif (number % 5).zero?
+    'buzz'
+  else
+    number.to_s
+  end
+end
