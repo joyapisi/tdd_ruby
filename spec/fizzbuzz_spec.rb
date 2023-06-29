75ba513b358b83960dde07664126ba45af9d71a0
Joy Phoebe <joyapisi@gmail.com>
2023-06-29 10:59:33 +0300

    update fizzbuzz test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,5 +9,13 @@ describe '#fizzbuzz' do
     expect(fizzbuzz(20)).to eq("buzz")
   end
 
+  it 'returns "fizzbuzz" when given a number divisible by both 3 and 5' do
+    expect(fizzbuzz(15)).to eq("fizzbuzz")
+    expect(fizzbuzz(30)).to eq("fizzbuzz")
+  end
 
+  it 'returns the number as a string when given a number not divisible by 3 or 5' do
+    expect(fizzbuzz(7)).to eq("7")
+    expect(fizzbuzz(13)).to eq("13")
+  end
 end
