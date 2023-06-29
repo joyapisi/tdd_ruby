cdcb3c5b54d409f04ee79227348ade627de23cc5
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-06-30 00:14:14 +0100

    Corrections

--- solver.rb
+++ solver.rb
@@ -1,23 +1,18 @@
-require_relative 'raise_exception'
-class Solver
-  def initialize
-    @exception = RaiseException.new
-  end
+# frozen_string_literal: true
 
+
+class Solver
   def factorial(num)
-    @exception.message('factorial') if num.negative?
     return 1 if [1, 0].include?(num)
 
     num * factorial(num - 1)
   end
 
-  def reverse(word)
-    @exception.message('reverse') unless word.instance_of?(String)
-    word.reverse!
+  def reverse(str)
+    str.reverse
   end
 
   def fizzbuzz(num)
-    @exception.message('fizzbuzz') unless num.instance_of?(Integer)
     if (num % 3).zero? && (num % 5).zero?
       'fizzbuzz'
     elsif (num % 3).zero?
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../solver'
 
 describe Solver do
